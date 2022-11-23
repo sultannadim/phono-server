8c09aefb363bfacb39774f845b615e7e41f91d95
sultan5218 <sultan5218@gmail.com>
2022-11-24 01:32:34 +0600

    get categories complete

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const app = express();
 const cors = require("cors");
+const { MongoClient, ServerApiVersion } = require("mongodb");
 require("dotenv").config();
 const port = process.env.PORT || 5000;
 
@@ -10,5 +11,30 @@ app.use(express.json());
 app.get("/", (req, res) => {
   res.send("Phono server api is running");
 });
+
+// mongodb part start
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.newitlb.mongodb.net/?retryWrites=true&w=majority`;
+
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
+async function run() {
+  try {
+    const categoryesCollection = client.db("phonoDb").collection("categories");
+    // get categories
+    app.get("/categories", async (req, res) => {
+      const query = {};
+      const category = await categoryesCollection.find(query).toArray();
+      res.send(category);
+    });
+  } finally {
+  }
+}
+run().catch((error) => console.error(error));
+
+// mongodb part end
 
 app.listen(port, console.log(`Surver running from port : ${port}`));
