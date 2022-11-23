2cebe0b802463a11a2db95f5a92419ea560e6b4a
sultan5218 <sultan5218@gmail.com>
2022-11-23 22:36:30 +0600

    server created

---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const app = express();
+const cors = require("cors");
+require("dotenv").config();
+const port = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("Phono server api is running");
+});
+
+app.listen(port, console.log(`Surver running from port : ${port}`));
